f6e0f9a90b3dcbe2528e2215ad3487eef683d932
mrpie <vitaliy@ivanou.by>
2019-03-22 19:30:47 +0300

    fix

--- registration.php
+++ registration.php
@@ -14,6 +14,6 @@
 
 \Magento\Framework\Component\ComponentRegistrar::register(
     \Magento\Framework\Component\ComponentRegistrar::MODULE,
-    'Onilab_Backend',
+    'Onilab_GoogleChartsFix',
     __DIR__
 );
